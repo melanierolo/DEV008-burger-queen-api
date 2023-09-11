99c71c016fde144657d025f662b833eb7a258b3c
Melanie Rodas <mrodasl@uni.pe>
2023-09-11 18:33:29 -0500

    added morgan -it logs information about incoming request

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const config = require('./config');
 const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
@@ -15,6 +16,7 @@ app.set('pkg', pkg);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(authMiddleware(secret));
+app.use(morgan('dev'));
 
 // Registrar rutas
 routes(app, (err) => {
